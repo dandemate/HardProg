8d91f102ae7215b85239eba50efd23c67f333194
Máté Dandé <dandemate94@hotmail.hu>
2020-03-02 13:23:45 +0100

    Add files via upload

---
+++ Feladat4.s
@@ -0,0 +1,15 @@
+# A program egy negatív számból kettes komplemensének visszafejtésével állítsa elő az ellentetjét!
+#	pl. -33 -> 33
+#		0xC6 -> 58
+#		0B11101111 -> 17
+
+            .intel_syntax noprefix
+            .globl main, x
+x:          .4byte  0B11101111
+
+main:
+    mov     eax, DWORD PTR [x]
+#   neg     eax                  # egyszerűbb
+    not     eax                 # egyes komplemens
+    add     eax, 1              # kettes komplemens
+    ret
