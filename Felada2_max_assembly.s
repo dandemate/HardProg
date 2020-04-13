65a3ec6998a236a444fa5c9a88bba306add7619b
Máté Dandé <dandemate94@hotmail.hu>
2020-04-13 19:22:07 +0200

    Update Felada2_max_assembly.s

--- Felada2_max_assembly.s
+++ Felada2_max_assembly.s
@@ -1,11 +1,17 @@
-        .intel_syntax noprefix
+# Írjunk egy függvényt, melynek első paramétere egy integer tömb kezdőcíme, második pedig
+# a tömbelemek száma. A függvény térjen vissza a tömb legnagyobb elemének értékével! Hívjuk
+# meg ezt a függvényt egy C programból.
+#
+# Dandé Máté
+		
+		.intel_syntax noprefix
         .globl  max
 
 max:
         push    rbp
         mov     rbp, rsp
-        sub     rsp, 16
-        mov     DWORD PTR[rbp-16], edi          # kezdőcím
+        sub     rsp, 20
+        mov     DWORD PTR[rbp-20], edi          # kezdőcím 
         mov     DWORD PTR[rbp-12], esi          # elemszám
         mov     DWORD PTR[rbp-8], 0             # i
         mov     DWORD PTR[rbp-4], 0             # max
@@ -15,14 +21,15 @@ max:
         pop     rbp
         ret
 
-BODY:   mov     ebx, DWORD PTR[rbp-8]                   # i
+BODY:   mov     ebx, DWORD PTR[rbp-8]
+        mov     eax, ebx                                # i
         mov     ecx, DWORD PTR[rbp-4]                   # max
         cdqe                                            
-        mov     eax, DWORD PTR[rbp-16+rbx*4]            # kezdőcím + i*4  (aktuális elem) 
+        mov     eax, DWORD PTR[rdi+rax*4]               # kezdőcím + i*4  (aktuális elem) 
         cmp     ecx, eax                                # ecx-> max, eax -> tömb i. eleme
         jl      IF                                      # ha max kisebb mint tomb i. eleme
         inc     ebx                                     
-        mov     DWORD PTR[rbp-8], ebx                           
+        mov     DWORD PTR[rbp-8], ebx                          
 
 COND:   cmp     ebx, esi
         jl      BODY
